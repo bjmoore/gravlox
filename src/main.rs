734507fd436ffcb2844bbb81290de03c947f38e5
Benjamin Moore <benjaminjm@gmail.com>
2024-10-19 10:14:45 -0700

    Stub out actual interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use chunk::Chunk;
+use clap::Parser;
 use op::{OP_ADD, OP_NEGATE, OP_RETURN, OP_SUBTRACT};
-use std::error::Error;
+use std::{
+    error::Error,
+    fs,
+    io::{self, Write},
+};
 use vm::GravloxVM;
 
 mod chunk;
@@ -8,17 +13,40 @@ mod op;
 mod value;
 mod vm;
 
+#[derive(Parser, Debug)]
+struct Args {
+    filename: Option<String>,
+
+    #[arg(short, long)]
+    debug: bool,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut vm = GravloxVM::new();
-    let mut chunk = Chunk::new("demo");
 
-    chunk.add_constant(3.14159, 2)?;
-    chunk.add_constant(3.14159, 2)?;
-    chunk.add_code(OP_SUBTRACT, 2);
-    chunk.add_code(OP_RETURN, 100);
-    println!("{}", chunk);
+    let args = Args::parse();
 
-    vm.interpret(&chunk);
+    match args.filename {
+        Some(filename) => run_script(&filename),
+        None => repl(),
+    }?;
 
     Ok(())
 }
+
+fn run_script(filename: &str) -> Result<(), Box<dyn Error>> {
+    let script = String::from_utf8(fs::read(filename)?)?;
+    Ok(run(&script))
+}
+
+fn repl() -> Result<(), Box<dyn Error>> {
+    loop {
+        let mut buf = String::new();
+        io::stdout().write_all(b"> ")?;
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut buf)?;
+        run(&buf);
+    }
+}
+
+fn run(source: &str) {}
