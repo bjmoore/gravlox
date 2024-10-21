4e94f35af14f542e7aa2c4baa46726ea76214558
Benjamin Moore <benjaminjm@gmail.com>
2024-10-21 07:46:01 -0700

    Start stubbing out test facility

--- src/bin/external_test.rs
+++ src/bin/external_test.rs
@@ -1,17 +1,25 @@
 use regex::Regex;
+use std::error::Error;
+use std::fs;
 use std::process::Command;
 
-fn main() {
+const GRAVLOX_PATH: &str = "./target/debug/gravlox";
+const TEST_DIR: &str = "./test";
+
+fn main() -> Result<(), Box<dyn Error>> {
     //foreach test file:
     // read in the test file
     // parse out all the expectations
     // feed test file to gravlox
     // consume stdout and check the expects in order
-    let expect_regex = Regex::new("//.*");
+    let expect_regex = Regex::new("// expect: (.*)")?;
+    for test_file in fs::read_dir(TEST_DIR)? {
+        let contents = fs::read_to_string(test_file?.path())?;
+        for line in contents.lines() {
+            let captures = expect_regex.captures(line).unwrap();
+            println!("{:?}", captures);
+        }
+    }
 
-    let mut command = Command::new("./target/debug/gravlox");
-
-    let output = command.arg("./test/basic").output().unwrap();
-
-    println!("{:?}", output);
+    Ok(())
 }
