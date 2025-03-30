9c587e8846a876ede5c48ed98285064cc4d53ff3
Benjamin Moore <benjaminjm@gmail.com>
2025-03-29 22:48:26 -0700

    Update the tester to handle multiple expectations

--- src/bin/external_test.rs
+++ src/bin/external_test.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::collections::VecDeque;
 use std::error::Error;
 use std::fs;
 use std::io::BufRead;
@@ -10,31 +11,43 @@ const TEST_DIR: &str = "./test";
 fn main() -> Result<(), Box<dyn Error>> {
     let expect_regex = Regex::new("// expect: (.*)")?;
     for test_file in fs::read_dir(TEST_DIR)? {
+        let mut passing = true;
+
         let test_file = test_file?;
         let test_name = test_file.file_name().into_string().unwrap();
         print!("Running test: {} ... ", test_name);
         let contents = fs::read_to_string(test_file.path())?;
 
-        let mut expectations = Vec::new();
+        let mut expectations = VecDeque::new();
 
         // put these into a vec
         for line in contents.lines() {
             if let Some(captures) = expect_regex.captures(line) {
                 if let Some(expected_value) = captures.get(1) {
-                    expectations.push(expected_value.as_str());
+                    expectations.push_back(expected_value.as_str());
                 }
             }
         }
 
         // compare vecs elt by elt and check they are =
         let output = Command::new(GRAVLOX_PATH).arg(test_file.path()).output()?;
+        let mut failed_expectations = Vec::new();
         for line in output.stdout.lines() {
             let line = line.unwrap();
             if expectations[0] != line {
-                println!("\x1b[0;31mfail!\x1b[0m");
-                println!("Expected: {}, Actual: {}", expectations[0], line);
+                passing = false;
+                failed_expectations.push(format!("Expected: {}, Actual: {}", expectations[0], line));
             } else {
-                println!("pass!");
+                expectations.pop_front();
+            }
+        }
+
+        if passing {
+            println!("pass!");
+        } else {
+            println!("\x1b[0;31mfail!\x1b[0m");
+            for failure in failed_expectations {
+                println!("{}", failure);
             }
         }
     }
